7d620f30b91762dcbb120bae2ddb39a6ebb8009f
Shalini <shalinipandeyfeb@gmail.com>
2022-08-02 00:19:44 +0530

    Tip-calculater

---
+++ Tip-calculator.js
@@ -0,0 +1,29 @@
+//the function called when Calculate button is clicked.
+window.onload = () => {
+    /*calling a function calculateTip which
+     will calculate the tip for the bill.*/
+    document.querySelector('#calculate').onclick =
+        calculateTip;
+}
+
+function calculateTip() {
+    /*assign values of ID : amount, person and
+    service to variables for further calculations.*/
+    let amount = document.querySelector('#amount').value;
+    let persons = document.querySelector('#persons').value;
+    let service = document.querySelector('#services').value;
+
+    console.log(service);
+    if (amount === '' && service === 'Select') {
+        alert("Please enter valid values");
+        return;
+    }
+    if (persons === '1')
+        document.querySelector('#each').style.display = 'none';
+    else
+        document.querySelector('#each').style.display = 'block';
+    let total = (amount * service) / persons;
+    total = total.toFixed(2);
+    document.querySelector('.tip').style.display = 'block';
+    document.querySelector('#total').innerHTML = total;
+}
